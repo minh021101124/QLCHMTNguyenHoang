0fe4c2c510ba9051a8eacaf84a3dca1d935b8d2d
Danh333 <danh27606@gmail.com>
2023-11-07 08:57:06 +0700

    QLCHMT

---
+++ QLCHMayTinhNguyenHoang/QLCHMayTinhNguyenHoang/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace QLCHMayTinhNguyenHoang
+{
+    partial class DangNhap
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TaiKhoan = new System.Windows.Forms.Label();
+            this.MatKhau = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // TaiKhoan
+            // 
+            this.TaiKhoan.AutoSize = true;
+            this.TaiKhoan.Location = new System.Drawing.Point(55, 97);
+            this.TaiKhoan.Name = "TaiKhoan";
+            this.TaiKhoan.Size = new System.Drawing.Size(58, 15);
+            this.TaiKhoan.TabIndex = 0;
+            this.TaiKhoan.Text = "Tài khoản:";
+            // 
+            // MatKhau
+            // 
+            this.MatKhau.AutoSize = true;
+            this.MatKhau.Location = new System.Drawing.Point(62, 175);
+            this.MatKhau.Name = "MatKhau";
+            this.MatKhau.Size = new System.Drawing.Size(57, 15);
+            this.MatKhau.TabIndex = 1;
+            this.MatKhau.Text = "Mật khẩu:";
+            // 
+            // DangNhap
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(331, 302);
+            this.Controls.Add(this.MatKhau);
+            this.Controls.Add(this.TaiKhoan);
+            this.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "DangNhap";
+            this.Text = "Đăng Nhập";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label TaiKhoan;
+        private System.Windows.Forms.Label MatKhau;
+    }
+}
+
